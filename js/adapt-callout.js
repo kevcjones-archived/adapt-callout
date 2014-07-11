9b9da7e500c60c9112d5a8df7eb43bb4c52f3bf8
KevCJones <him@technicalsamurai.com>
2014-07-11 12:41:23 +0100

    Dog rough first example - callout item triggers the simplest popup using notifier

--- js/adapt-callout.js
+++ js/adapt-callout.js
@@ -1,5 +1,5 @@
 /*
-* adapt-component
+* adapt-callout
 * License - http://github.com/adaptlearning/adapt_framework/LICENSE
 * Maintainers - Kevin Jones <him@kevincjones.co.uk>
 */
@@ -7,6 +7,12 @@
 define(["coreViews/componentView", "coreJS/adapt"], function(ComponentView, Adapt) {
 
     var Callout = ComponentView.extend({
+
+        events: function () {
+            return {
+                'click .callout-trigger':'openCallout'
+            }
+        },
         
         postRender: function() {
             console.log("rendering");
@@ -22,7 +28,23 @@ define(["coreViews/componentView", "coreJS/adapt"], function(ComponentView, Adap
             // Use this to set the model status to complete.
             // This can be used with inview or when the model is set to complete/the question has been answered.
             this.setCompletionStatus();
+        },
+
+        openCallout: function (event) {
+            event.preventDefault();
+            // var currentCallout = $(event.currentTarget).data('id'); //data-id
+            // this.$('.callout-item').hide().removeClass('active'); //hide all
+            // this.$('.'+currentCallout).show().addClass('active'); //show only clicked one
+            console.log("clicked");
+            var calloutobject = {
+                title: "Title Goes Here",
+                body:"<img style='float:left;margin:5px' src='http://placehold.it/300x300'><p>    !Arne Astrup is Head of the <a class=\"callout-trigger\" href=\"javascript:void(0);\">Department of Nutrition,</a> Exercise and Sports at the Faculty of Science, University of Copenhagen, Denmark. He is Director of the    Nordea Foundation funded OPUS research centre.</p>"
+            };
+
+            Adapt.trigger('notify:popup',calloutobject);
         }
+
+        
         
     });
     
